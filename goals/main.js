cb65e52d8fa3ba7f7ca8005df8bc3ad1cec1bae6
Kim <kimberlybicknese@Kimberlys-MacBook-Pro.local>
2016-10-24 14:19:11 -0400

    madlibs homework

---
+++ goals/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by kimberlybicknese on 10/13/16.
+ */
+
+var movies = ["Spirited Away", "Cinderella Man", "Beautiful Mind", "Pitch Perfect", "Green Mile"];
+
+movies.unshift("Howl's Moving Castle");
+movies.push("Kiki's Delivery Service");
+
+console.log(movies);
+
---
+++ madlibs/madlib-console.js
@@ -0,0 +1,64 @@
+/**
+ * Created by kimberlybicknese on 10/19/16.
+ */
+
+
+/*
+
+This function will continuosly generate random company names in the sentence when clicking the
+"create new startup" button.
+
+*/
+
+function displayStartup() {
+
+    // create array for startup companies
+
+    var startupX = ["Uber", "Google", "Amazon", "Twitter", "Facebook", "Clutch", "Classpass", "BarkBox", "Pinterest", "Instagram", "Apple", "Waze", "HelloFresh", "Blue Apron", "Plated", "Yelp"];
+    var startupY = ["kittens", "puppies", "hipsters", "gamers", "geeks", "dogs", "cats", "artists", "engineers", "scientists", "poodles", "parents", "kids", "doctors"];
+
+    // Variables will randomly choose one name out of each array
+
+    var random1 = Math.floor((Math.random() * startupX.length));
+    var random2 = Math.floor((Math.random() * startupY.length));
+
+    // Create string for the output and print to the h1
+
+    var result = "A startup that is like " + startupX[random1] + ", but for " + startupY[random2] + ".";
+    document.getElementById("XforY").innerHTML = result;
+}
+
+// Clicking on a button will place the madlib saying in the h1 tag
+
+document.getElementById("button").onclick = displayStartup;
+
+// Clicking on "favorite" will save the saying into a favorites bucket
+
+function saveStartup() {
+    var favoriteStartups = [];
+    for ( var i = 0; i < favoriteStartups.length; i++ ) {
+        var favoriteElement = document.getElementById("XforY").innerHTML = result;
+        favoriteStartups.push( favoriteElement );
+        document.getElementById("favoriteList").innerHTML = favoriteStartups[i];
+    }
+}
+
+document.getElementById("favorite").onclick = saveStartup;
+
+// Clicking on "show favorites" will toggle on or off all of your favorite madlibs sayings in a div that is
+// initially hidden
+
+function showFavorites() {
+    document.getElementById("favoriteList").classList.toggle("show");
+    document.getElementById("showFavorites").classList.toggle("toggle");
+}
+
+document.getElementById("showFavorites").onclick = showFavorites;
+
+
+
+
+
+
+
+
