a53db7636c2b3a4d23e110deb9e7cafd2fe23b50
Kim <kimberlybicknese@Kimberlys-MacBook-Pro.local>
2016-12-07 09:59:51 -0500

    making json file for properties

---
+++ final/js/properties.js
@@ -0,0 +1,59 @@
+/**
+ * Created by kimberlybicknese on 12/6/16.
+ */
+
+var properties  = {
+    "properties":[
+        {
+            "price": "$185,000",
+            "address": "2567 Hatfield Cir",
+            "city": "Atlanta",
+            "state": "GA",
+            "zipcode": "30316",
+            "img": "http://img12.cinccdn.com/201612/MLS/FMLS/489/FMLS-RESALL-44093489-1.jpg",
+            "status": "active",
+            "roof": "Composition",
+            "style": "Traditional",
+            "area": 3148,
+            "bedrooms": 3,
+            "bathsFull": 2,
+            "bathsHalf": 1,
+            "stories": 2,
+            "fireplaces": 1,
+            "heating": "Central Gas",
+            "interiorFeatures": "Alarm System, Breakfast Bar",
+            "lotSize": "8425",
+            "exteriorFeatures": "Back Yard, Fenced",
+            "subdivision": "Lakes/Avalon Village Sec 1",
+            "type": "RES",
+            "yearBuilt": 2006,
+            "sqft": 1467
+        },
+        {
+            "price": "$185,000",
+            "address": "2567 Hatfield Cir",
+            "city": "Atlanta",
+            "state": "GA",
+            "zipcode": "30316",
+            "img": "http://img12.cinccdn.com/201612/MLS/FMLS/489/FMLS-RESALL-44093489-1.jpg",
+            "status": "active",
+            "roof": "Composition",
+            "style": "Traditional",
+            "area": 3148,
+            "bedrooms": 3,
+            "bathsFull": 2,
+            "bathsHalf": 1,
+            "stories": 2,
+            "fireplaces": 1,
+            "heating": "Central Gas",
+            "interiorFeatures": "Alarm System, Breakfast Bar",
+            "lotSize": "8425",
+            "exteriorFeatures": "Back Yard, Fenced",
+            "subdivision": "Lakes/Avalon Village Sec 1",
+            "type": "RES",
+            "yearBuilt": 2006,
+            "sqft": 1467
+        }
+    ]
+
+};
--- final/js/scripts.js
+++ final/js/scripts.js
@@ -4,6 +4,10 @@
 
 // Functions
 
+filterToggle();
+
+properties();
+
 // Find height of header and subtract it from search section div height
 
 $( document ).ready(function() {
@@ -12,12 +16,74 @@ $( document ).ready(function() {
     $('#searchLayout').css('height', searchHeight);
 });
 
+// Make property prototype
+
+function propertyProto(address, city, state, zipcode, price, beds, baths, sqft, imageURL, status) {
+    var property = {
+        address: address,
+        city: city,
+        state: state,
+        zipcode: zipcode,
+        price: price,
+        beds: beds,
+        baths: baths,
+        sqft: sqft,
+        image: imageURL,
+        status: status
+    };
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zipcode = zipcode;
+    this.price = price;
+    this.beds = beds;
+    this.baths = baths;
+    this.sqft = sqft;
+    this.imageURL = imageURL;
+    this.status = status;
+    this.property = property
+}
+
+// Print results to the page using handlebars
+
+function properties() {
+    var url = "js/properties.json";
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(result) {
+        var propertyResults = result.properties;
+
+        $.each(propertyResults, function(index, item){
+
+            var source = $("#properties").html();
+            var template = Handlebars.compile(source);
+
+            // Define properties with handlebars
+
+            var propertyCard = new propertyProto(item.address, item.city, item.state, item.zipcode, item.price, item.beds, item.baths, item.sqft, item.img, item.status);
+
+            var html = template(propertyCard.property);
+
+            // Print articles to the #main container
+
+            $(".properties").append(html);
+
+            console.log(result);
+        });
+    }).fail(function(err) {
+        throw err;
+    });
+
+
+}
+
 // When user clicks on filters button, more filters panel will toggle, covering the listings area until the user
 // exits out or applies new filters.
 
 function filterToggle() {
     $("#searchFilters").on("click", function() {
-        $(".filter-panel").toggle();
+        $(".filter-panel").toggle(200);
     })
 }
 
